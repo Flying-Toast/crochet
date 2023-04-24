90016bad4ad4dd6343ba895d3f3d01ade64a11b1
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2023-04-24 13:53:52 -0400

    stitch input/output count of instruction

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,50 @@ pub enum Instruction {
     Repeat(Box<Instruction>, u32),
 }
 
+impl Instruction {
+    /// How many stitches this instruction consumes.
+    ///
+    /// Example:
+    /// ```
+    /// # use crochet::Instruction;
+    ///
+    /// assert_eq!(Instruction::Inc.input_count(), 1);
+    /// assert_eq!(Instruction::Dec.input_count(), 2);
+    /// ```
+    pub fn input_count(&self) -> u32 {
+        use Instruction::*;
+
+        match self {
+            Sc => 1,
+            Inc => 1,
+            Dec => 2,
+            Group(insts) => insts.iter().map(Self::input_count).sum(),
+            Repeat(inst, times) => inst.input_count() * times,
+        }
+    }
+
+    /// How many stitches this instruction creates.
+    ///
+    /// Example:
+    /// ```
+    /// # use crochet::Instruction;
+    ///
+    /// assert_eq!(Instruction::Sc.output_count(), 1);
+    /// assert_eq!(Instruction::Inc.output_count(), 2);
+    /// ```
+    pub fn output_count(&self) -> u32 {
+        use Instruction::*;
+
+        match self {
+            Sc => 1,
+            Inc => 2,
+            Dec => 1,
+            Group(insts) => insts.iter().map(Self::output_count).sum(),
+            Repeat(inst, times) => inst.output_count() * times,
+        }
+    }
+}
+
 pub fn parse_rounds(source: &str) -> Result<Vec<Instruction>, (usize, usize)> {
     let mut ts = lex::tokenize(source);
 
